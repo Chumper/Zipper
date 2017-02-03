c954d6bd8396c3e5e7e7a3d3f96ce58d23d8a2e9
Vitaliy Ryaboy <vriaboy@gmail.com>
2017-02-03 15:25:59 +0100

    configured PHP Coding Standards Fixer (#95)
    * migrate all expectedException* annotation in favor of methods
    * added .php_cs for v2.0

--- tests/Repositories/ZipRepositoryTest.php
+++ tests/Repositories/ZipRepositoryTest.php
@@ -2,9 +2,9 @@
 
 namespace Chumper\Zipper\Repositories;
 
+use Exception;
 use Mockery;
 use ZipArchive;
-use Exception;
 
 /**
  * Created by JetBrains PhpStorm.
@@ -13,10 +13,8 @@ use Exception;
  * Time: 20:57
  * To change this template use File | Settings | File Templates.
  */
-
 class ZipRepositoryTest extends \PHPUnit_Framework_TestCase
 {
-
     /**
      * @var ZipRepository
      */
@@ -29,8 +27,13 @@ class ZipRepositoryTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->mock = Mockery::mock(new ZipArchive);
+        $this->mock = Mockery::mock(new ZipArchive());
         $this->zip = new ZipRepository('foo', true, $this->mock);
+    }
+
+    protected function tearDown()
+    {
+        Mockery::close();
     }
 
     public function testMake()
@@ -39,22 +42,18 @@ class ZipRepositoryTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($zip->fileExists('foo'));
     }
 
-    /**
-     * @expectedException Exception
-     * @expectedExceptionMessage Error: Failed to open idonotexist.zip! Error: ZipArchive::ER_
-     */
     public function testOpenNonExistentZipThrowsException()
     {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Error: Failed to open idonotexist.zip! Error: ZipArchive::ER_');
         new ZipRepository('idonotexist.zip', false);
     }
 
-    /**
-     * @expectedException Exception
-     * @expectedExceptionMessageRegExp /Error: Failed to open (.*)ZipRepositoryTest.php! Error: ZipArchive::ER_NOZIP - Not a zip archive./
-     */
     public function testOpenNonZipThrowsException()
     {
-        new ZipRepository(__DIR__ . DIRECTORY_SEPARATOR . 'ZipRepositoryTest.php', false);
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessageRegExp('/Error: Failed to open (.*)ZipRepositoryTest.php! Error: ZipArchive::ER_NOZIP - Not a zip archive./');
+        new ZipRepository(__DIR__.DIRECTORY_SEPARATOR.'ZipRepositoryTest.php', false);
     }
 
     public function testAddFile()
@@ -84,8 +83,8 @@ class ZipRepositoryTest extends \PHPUnit_Framework_TestCase
         $this->mock->shouldReceive('getFromName')->once()
             ->with('foo/bar')->andReturn('baz');
 
-        $this->assertEquals('foo', $this->zip->getFileContent('bar'));
-        $this->assertEquals('baz', $this->zip->getFileContent('foo/bar'));
+        $this->assertSame('foo', $this->zip->getFileContent('bar'));
+        $this->assertSame('baz', $this->zip->getFileContent('foo/bar'));
     }
 
     public function testGetFileStream()
@@ -95,8 +94,8 @@ class ZipRepositoryTest extends \PHPUnit_Framework_TestCase
         $this->mock->shouldReceive('getStream')->once()
             ->with('foo/bar')->andReturn('baz');
 
-        $this->assertEquals('foo', $this->zip->getFileStream('bar'));
-        $this->assertEquals('baz', $this->zip->getFileStream('foo/bar'));
+        $this->assertSame('foo', $this->zip->getFileStream('bar'));
+        $this->assertSame('baz', $this->zip->getFileStream('foo/bar'));
     }
 
     public function testFileExists()
@@ -114,11 +113,4 @@ class ZipRepositoryTest extends \PHPUnit_Framework_TestCase
     {
         $this->zip->close();
     }
-
-    protected function tearDown()
-    {
-        Mockery::close();
-    }
-
-
 }
